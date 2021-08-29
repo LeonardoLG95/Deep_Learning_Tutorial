14d44b9e788d03b38b60590d6b311b6a09c2d262
Leo <leonardolg95m@gmail.com>
2021-08-29 22:43:44 +0200

    chore: adding exercisey

--- 5 - exercise_flowers_classification.py
+++ 5 - exercise_flowers_classification.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-
 # Descargar los datos
 URL = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 zip_file = tf.keras.utils.get_file(origin=URL,
@@ -16,3 +15,126 @@ base_dir = os.path.join(os.path.dirname(zip_file), 'flower_photos')
 
 classes = ['roses', 'daisy', 'dandelion', 'sunflowers', 'tulips']
 
+train_number = 0
+val_number = 0
+# Divide las imagenes en directorios de validacion y entrenamiento, dentro cada tipo de flor
+for cl in classes:
+    img_path = os.path.join(base_dir, cl)
+    images = glob.glob(img_path + '/*.jpg')
+    print(f"{cl}: {len(images)} Images")
+    train, val = images[:round(len(images) * 0.8)], images[round(len(images) * 0.8):]
+
+    for t in train:
+        if not os.path.exists(os.path.join(base_dir, 'train', cl)):
+            os.makedirs(os.path.join(base_dir, 'train', cl))
+        if not os.path.exists(os.path.join(base_dir, 'val', cl, t)):
+            shutil.move(t, os.path.join(base_dir, 'train', cl))
+        train_number += 1
+
+    for v in val:
+        if not os.path.exists(os.path.join(base_dir, 'val', cl)):
+            os.makedirs(os.path.join(base_dir, 'val', cl))
+        if not os.path.exists(os.path.join(base_dir, 'val', cl, v)):
+            shutil.move(v, os.path.join(base_dir, 'val', cl))
+        val_number += 1
+
+print(f'Total de imagenes de entrenamiento : {train_number}')
+print(f'Total de imagenes de validacion : {val_number}')
+train_dir = os.path.join(base_dir, 'train')
+val_dir = os.path.join(base_dir, 'val')
+
+
+# This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
+def plot_images(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+    plt.tight_layout()
+    plt.show()
+
+
+BATCH_SIZE = 50
+IMG_SHAPE = 100
+
+# Agregar a las imagenes transformacion (augmentation):
+#   channel shift range: cambia el rango de colores de forma aleatoria
+#   brightness range: cambia aleatoriamente el brillo
+image_gen_train = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        channel_shift_range=0.5,
+        horizontal_flip=True,
+        brightness_range=(0.7, 1),
+        fill_mode='reflect')
+train_data_gen = image_gen_train.flow_from_directory(batch_size=BATCH_SIZE,
+                                                     directory=train_dir,
+                                                     shuffle=True,
+                                                     target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                     class_mode='binary')
+augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+plot_images(augmented_images)
+
+# Imagenes de validacion
+image_gen_val = ImageDataGenerator(rescale=1./255)
+val_data_gen = image_gen_val.flow_from_directory(batch_size=BATCH_SIZE,
+                                                 directory=val_dir,
+                                                 target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                 class_mode='binary')
+
+# Creando el modelo
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_SHAPE, IMG_SHAPE, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='softmax'),
+    tf.keras.layers.Dense(5)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+# Entrenando
+epochs = 200
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=int(np.ceil(train_number / float(BATCH_SIZE))),
+    epochs=epochs,
+    validation_data=val_data_gen,
+    validation_steps=int(np.ceil(val_number / float(BATCH_SIZE)))
+)
+
+# Visualizar el resultado del entrenamiento
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs_range = range(epochs)
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.savefig('./5-execise_flowers-200.jpg')
+plt.show()
