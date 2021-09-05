0e1cc2ba292d08bb17a0674dd5c22cf673be1df6
Leo <leonardolg95m@gmail.com>
2021-09-05 13:07:23 +0200

    chore: adding more comments

--- 6 - transfer_learning_cats_dogs.py
+++ 6 - transfer_learning_cats_dogs.py
@@ -12,10 +12,21 @@ import logging
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
-# Descargar el modelo mobilenet
+# Descargar el modelo mobilenet, este modelo espera imagenes de 224 x 224 px
 CLASSIFIER_URL = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
 IMAGE_RES = 224
 
 model = tf.keras.Sequential([
     hub.KerasLayer(CLASSIFIER_URL, input_shape=(IMAGE_RES, IMAGE_RES, 3))
 ])
+
+# El modelo esta pensado para tener 1000 outputs diferentes (cosas que puede detectar)
+#   y entre ellas estan los uniformes militares, como esta imagen que vamos a descargar
+grace_hopper = tf.keras.utils.get_file('image.jpg',
+                                       'https://storage.googleapis.com/download.tensorflow.org/'
+                                       'example_images/grace_hopper.jpg')
+grace_hopper = Image.open(grace_hopper).resize((IMAGE_RES, IMAGE_RES))
+grace_hopper = np.array(grace_hopper)/255.0
+result = model.predict(grace_hopper[np.newaxis, ...])
+
+predicted_class = np.argmax(result[0], axis=-1)
